671352d5d0ae954c6b61ed2585d8612e7bb883b9
Aarti R. Wasnik <aartiwasnik369@gmail.com>
2026-02-28 19:52:03 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,6 +0,0 @@
-function calc(){
-let bill=document.getElementById("bill").value
-let system=bill/1000
-document.getElementById("result").innerHTML=
-"You need approx "+system.toFixed(2)+" KW solar system"
-}
